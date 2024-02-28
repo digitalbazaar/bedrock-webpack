e580e87e29939defd31e4b5f83f2f56c862582b6
David I. Lehn <dlehn@digitalbazaar.com>
2024-02-28 18:28:49 -0500

    Updaet eslint deps and add GitHub lint action.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,13 +18,15 @@
 
 module.exports = {
   root: true,
-  parserOptions: {
-    // this is required for dynamic import()
-    ecmaVersion: 2020
-  },
   env: {
     node: true
   },
-  extends: ['digitalbazaar', 'digitalbazaar/jsdoc'],
-  ignorePatterns: ['node_modules/']
+  extends: [
+    'digitalbazaar',
+    'digitalbazaar/jsdoc',
+    'digitalbazaar/module'
+  ],
+  rules: {
+    'unicorn/prefer-node-protocol': 'error'
+  }
 };
